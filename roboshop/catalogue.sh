3d61fe081c9175f5f6cdd4d2a91655faee7bc7da
Hitesh Patel <hpatel3747@gmail.com>
2024-10-08 13:04:06 -0400

    deploy catalogue over bash script

--- roboshop/catalogue.sh
+++ roboshop/catalogue.sh
@@ -1,15 +1,19 @@
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
+
 useradd roboshop
+
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
 cd /app
 unzip /tmp/catalogue.zip
+
 cd /app
 npm install
-cp catalogue.service /etc/systemd/system/catalogue.service
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 
 dnf install mongodb-mongosh -y
 mongosh --host mongodb.hptldevops.online </app/db/master-data.js
